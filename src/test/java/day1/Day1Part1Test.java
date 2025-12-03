239faa8ee6d03e977be303fc4b0aee3b3cf9a303
NawaMan <nawa@nawaman.net>
2025-12-03 01:41:04 -0500

    Day 3

--- src/test/java/day1/Day1Part1Test.java
+++ src/test/java/day1/Day1Part1Test.java
@@ -1,17 +1,28 @@
 package day1;
 
 import static day1.Day1Part1Test.Direction.L;
-import static functionalj.list.FuncList.zipOf;
+
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
 import common.BaseTest;
+import functionalj.function.Func1;
 import functionalj.list.FuncList;
 import functionalj.types.Struct;
 
 public class Day1Part1Test extends BaseTest {
     
-    enum Direction { L, R }
+    enum Direction {
+        L, R;
+        static Direction valueOf(char ch) {
+            return switch (ch) {
+                case 'L': { yield L; }
+                case 'R': { yield R; }
+                default: throw new IllegalArgumentException("Unexpected value: " + ch);
+            };
+        }
+    }
     
     @Struct
     static interface RotationSpec {
@@ -23,18 +34,31 @@ public class Day1Part1Test extends BaseTest {
         }
     }
     
+    static Func1<String, Rotation> toRotation() {
+        return str -> {
+            var dir = Direction.valueOf(str.charAt(0));
+            var dis = Integer.parseInt(str.substring(1));
+            return new Rotation(dir, dis);
+        };
+    }
+    
     Object calculate(FuncList<String> lines) {
-        var directions = lines.flatMap(grab(regex("L|R")))   .map(Direction::valueOf);
-        var distances  = lines.flatMap(grab(regex("[0-9]+"))).map(Integer::valueOf);
-        var rotations  = zipOf(directions, distances, Rotation::new);
-        int current = 50;
-        int zeros   =  0;
-        for (var rotation : rotations) {
-            var displament = rotation.displament();
-            current = (current+ displament + 100) % 100;
-            if (current == 0) zeros++;
-        }
-        return zeros;
+//        var rotations  = lines.map(toRotation());
+//        int current = 50;
+//        int zeros   =  0;
+//        for (var rotation : rotations) {
+//            var displament = rotation.displament();
+//            current = (current+ displament + 100) % 100;
+//            if (current == 0) zeros++;
+//        }
+//        return zeros;
+        return lines.stream()
+             .map(toRotation())
+             .mapToInt(Rotation::displament)
+             .prependWith(IntStream.of(50))
+             .accumulate((cur, d) -> (cur + d + 100) % 100)
+             .filter(c -> c == 0)
+             .count();
     }
     
     //== Test ==
---
+++ src/test/java/day3/Day3Part2Test.java
@@ -0,0 +1,66 @@
+package day3;
+
+import org.junit.Test;
+
+import common.BaseTest;
+import day3.Day3Part1Test.Found;
+import functionalj.list.FuncList;
+import functionalj.stream.intstream.IntStreamPlus;
+
+public class Day3Part2Test extends BaseTest {
+    
+    record Found(long value, int position) {}
+    
+    Found findLargestOf(int[] digits, int start, int end) {
+        var max = -1L;
+        var pos = -1;
+        for (int i = start; i < end; i++) {
+            var digit = digits[i];
+            if (digit > max) {
+                max = digit;
+                pos = i;
+            }
+        }
+        return new Found(max, pos);
+    }
+    
+    long largestOf(int[] digits, int start, int wantDigits) {
+        if (wantDigits == 0)
+            return 0;
+        
+        var found = findLargestOf(digits, start, digits.length - (wantDigits - 1));
+        var pow  = ((long)Math.pow(10, wantDigits - 1));
+        var next = largestOf(digits, found.position + 1, wantDigits - 1);
+        return found.value*pow + next;
+    }
+    
+    long largestOf(String line, int wantDigits) {
+        var digits = IntStreamPlus.from(line.chars()).map(c -> c - '0').toArray();
+        return largestOf(digits, 0, wantDigits);
+    }
+    
+    Object calculate(FuncList<String> lines) {
+        return lines
+                .mapToLong(line -> largestOf(line, 12))
+                .sum();
+    }
+    
+    //== Test ==
+    
+    @Test
+    public void testExample() {
+        var lines  = readAllLines();
+        var result = calculate(lines);
+        println("result: " + result);
+        assertAsString("3121910778619", result);
+    }
+    
+    @Test
+    public void testProd() {
+        var lines  = readAllLines();
+        var result = calculate(lines);
+        println("result: " + result);
+        assertAsString("169709990062889", result);
+    }
+    
+}
